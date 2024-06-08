77b60d45d6e1b67db63fca1be0968479d73d2637
Yashrajput19 <yr152535@gmail.com>
2024-06-08 23:39:34 +0530

    adding static notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Note from './components/Note'
+import { notes } from './notes.js'
 
 function App() {
 
   return (
     <>
       <Header/>
-      <Note/>
+
+      { notes.map((noteItem) => {
+        return (
+          <Note key={noteItem.key} title={noteItem.title} content={noteItem.content}/>
+        )
+      }) }
+      
       <Footer/>
     </>
   )
